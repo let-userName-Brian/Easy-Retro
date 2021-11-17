995d3057184fd9436b051ea8437a66bb649b15e3
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-17 17:00:48 -0500

    add knex integration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV || 'development']);
 const app = express();
 const port = 8080;
 
@@ -6,8 +7,12 @@ app.get('/', (req, res) => {
   res.status(200).send('Welcome to Super Mario World!');
 });
 
+app.get('/users', (req, res) => {
+  knex.select('*').from('user').then(data => res.json(data))
+})
+
 const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Dropping the hammer on port:${port}`);
 })
 
 module.exports = server;
