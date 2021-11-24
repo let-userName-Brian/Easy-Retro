c35bbeceb3332dc81f9e50548b7ccc5f5a1fd5f4
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-24 11:43:23 -0500

    fetch comments function

--- src/comments.js
+++ src/comments.js
@@ -1,5 +1,13 @@
+const { knex } = require('./knexConnector')
+const { fetchCardsByRetroId } = require('./cards')
+
 async function fetchCommentsByRetroId(retro_id) {
-  return []
+  let cards = await fetchCardsByRetroId(retro_id)
+
+  return knex('comment')
+    .join('user_profile', 'comment.user_id', '=', 'user_profile.user_id')
+    .whereIn('card_id', cards.map(card => card.card_id))
+    .select('comment.*', 'user_profile.user_name')
 }
 
-module.exports = { fetchCardsByRetroId: fetchCommentsByRetroId }
+module.exports = { fetchCommentsByRetroId }
