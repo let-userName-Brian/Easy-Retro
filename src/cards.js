5b2d995297d6da7e09e00ef9204978b34f652d3f
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-24 11:43:06 -0500

    add user_name to cards

--- src/cards.js
+++ src/cards.js
@@ -5,8 +5,9 @@ async function fetchCardsByRetroId(retro_id) {
   let columns = await fetchColumnsByRetroId(retro_id)
 
   return knex('card')
-    .select('*')
+    .join('user_profile', 'card.user_id', '=', 'user_profile.user_id')
     .whereIn('card_id', columns.flatMap(column => column.card_ids))
+    .select('card.*', 'user_profile.user_name')
 }
-//columns.card_ids
+
 module.exports = { fetchCardsByRetroId }
