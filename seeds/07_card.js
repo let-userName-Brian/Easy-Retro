0687e477d9ef7a16b828315558d936dc28b19040
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-24 11:17:06 -0500

    add mock text and make Mario mad at me

--- seeds/07_card.js
+++ seeds/07_card.js
@@ -1,11 +1,25 @@
+const loremHipsum = require('lorem-hipsum')
+
 exports.seed = function (knex) {
   let arr = []
+
   for (let card_id = 1; card_id < 50; card_id++) {
+    let votes = []
+    let maxVotes = Math.floor(Math.random() * 10)
+    for (let v = 0; v < maxVotes; v++) {
+      votes.push({ "0e5639ea-8868-4bf6-ab5f-cb8a8d470785": "up_vote" })
+    }
+
     arr.push({
       card_id,
-      card_text: "Four dollar toast photo booth chia selfies biodiesel occupy chambray VHS snackwave.",
+      card_text: loremHipsum({
+        count: 1,
+        units: 'sentences',
+        sentenceLowerBound: 5,
+        sentenceUpperBound: 15
+      }),
       user_id: "c1ad74ae-b651-4fa0-9820-833193797964",
-      votes: { "c1ad74ae-b651-4fa0-9820-833193797964": "up_vote" }
+      votes: JSON.stringify(votes)
     })
   }
   // Inserts seed entries
