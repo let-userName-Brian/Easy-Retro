e36b0def4e700a1e7c6bedf75485a9de6e19e307
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-29 15:47:53 -0500

    remove retro default name

--- migrations/20211117172650_retro.js
+++ migrations/20211117172650_retro.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('retro', table => {
     table.uuid('retro_id').primary();
-    table.string('retro_name').defaultTo('New Retro')
+    table.string('retro_name')
     table.specificType('column_ids', 'int[]') // array of column IDs, specifying which columns are in the retro, and what order e.g. [1, 3, 2]
     table.json('retro_options').defaultTo({}) // object of key-value pairs, defining options for the retro
     table.specificType('tags', 'text[]')
