6bfeafdf6ef762dec6a502a1b071a024d8b92866
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-12-01 16:43:14 -0500

    add voting funcs

--- src/votes.js
+++ src/votes.js
@@ -1,18 +1,45 @@
 const { knex } = require('./knexConnector')
 
 async function updateAddVote(user_id, card_id, vote_type) {
-  // return await knex.transaction(async (t) => {
-  //   return await t('card')
-  //     .select('votes')
-  //     .then(async (new_column_id) => knex('retro')
-  //       .where({ retro_id })
-  //       // .update('column_ids', knex.raw('column_ids || ?', [new_column_id]), 'column_ids'))
-  //       .update('column_ids', knex.raw('array_cat(column_ids, ?)', [new_column_id]), 'column_ids'))
-  // })
+  console.log('updateAddVote', user_id, card_id, vote_type)
+  return await knex.transaction(async (t) => {
+    return await t('card')
+      .where({ card_id })
+      .select('votes')
+      .then(cards => cards[0].votes)
+      .then(async (votes) => {
+        console.log('Current votes:', votes)
+        votes.push({ user_id, vote_type })
+        console.log('New votes:', votes)
+        t('card')
+          .where({ card_id })
+          .update('votes', JSON.stringify(votes), '*')
+          .then(cards => console.log('Card:', cards[0]))
+      })
+  }
+  )
 }
 
-async function updateRemoveVote() {
 
+async function updateRemoveVote(user_id, card_id, vote_type) {
+  console.log('updateRemoveVote', user_id, card_id, vote_type)
+  return await knex.transaction(async (t) => {
+    return await t('card')
+      .where({ card_id })
+      .select('votes')
+      .then(cards => cards[0].votes)
+      .then(async (votes) => {
+        console.log('Current votes:', votes)
+        votes.filter(vote => vote.user_id !== user_id && vote.vote_type !== vote_type)
+        console.log('New votes:', votes)
+        t('card')
+          .where({ card_id })
+          .update('votes', JSON.stringify(votes), '*')
+          .then(cards => console.log('Card:', cards[0]))
+
+      })
+  }
+  )
 }
 
 module.exports = { updateAddVote, updateRemoveVote }
