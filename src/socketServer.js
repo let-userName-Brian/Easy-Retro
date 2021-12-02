a428907284307459821b509cbd3813ec7d087c3a
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-12-02 14:24:59 -0500

    stored retro_id in correct scope
    Co-authored-by: galvanize-chasten-warwick <galvanize-chasten-warwick@users.noreply.github.com>
    Co-authored-by: ussmario <ussmario@users.noreply.github.com>
    Co-authored-by: stephenwallace356 <stephenwallace356@users.noreply.github.com>

--- src/socketServer.js
+++ src/socketServer.js
@@ -7,7 +7,6 @@ const { updateAddVote, updateRemoveVote } = require('./votes')
 
 module.exports = class SocketServer {
   constructor(server) {
-    this.retro_id = null
     this.io = socketIo(server, {
       cors: {
         origin: "*",
@@ -20,27 +19,29 @@ module.exports = class SocketServer {
     this.io.on('connection', (socket) => {
       this.io.emit('newConnection', socket.id)
 
+      let retro_id = null
+
       // Establish all connection points that the client may send to the server
-      socket.on('joinRetro', async (payload) => await this.joinRetro(socket, payload))
+      socket.on('joinRetro', async (payload) => retro_id = await this.joinRetro(socket, payload))
 
       // Listen for column events from the client
-      socket.on('addColumn', ({ retro_id }) => this.addColumn(retro_id))
-      socket.on('removeColumn', ({ retro_id, column_id }) => this.removeColumn(retro_id, column_id))
-      socket.on('renameColumn', ({ retro_id, column_id, column_name }) => this.renameColumn(retro_id, column_id, column_name))
+      socket.on('addColumn', ({ }) => this.addColumn(retro_id))
+      socket.on('removeColumn', ({ column_id }) => this.removeColumn(retro_id, column_id))
+      socket.on('renameColumn', ({ column_id, column_name }) => this.renameColumn(retro_id, column_id, column_name))
 
       // Listen for card events from the client
-      socket.on('addCard', ({ retro_id, column_id, user_id }) => this.addCard(column_id, user_id))
-      socket.on('removeCard', ({ card_id }) => this.removeCard(card_id))
-      socket.on('changeCardText', ({ card_id, card_text }) => this.changeCardText(card_id, card_text))
+      socket.on('addCard', ({ column_id, user_id }) => this.addCard(retro_id, column_id, user_id))
+      socket.on('removeCard', ({ card_id }) => this.removeCard(retro_id, card_id))
+      socket.on('changeCardText', ({ card_id, card_text }) => this.changeCardText(retro_id, card_id, card_text))
 
       // Listen for comment events from the client
-      socket.on('addComment', ({ card_id, user_id }) => this.addComment(card_id, user_id))
-      socket.on('removeComment', ({ comment_id, card_id }) => this.removeComment(comment_id, card_id))
-      socket.on('changeCommentText', ({ comment_id, commentText }) => this.changeCommentText(comment_id, commentText))
+      socket.on('addComment', ({ card_id, user_id }) => this.addComment(retro_id, card_id, user_id))
+      socket.on('removeComment', ({ comment_id, card_id }) => this.removeComment(retro_id, comment_id, card_id))
+      socket.on('changeCommentText', ({ comment_id, commentText }) => this.changeCommentText(retro_id, comment_id, commentText))
 
       // Listen for vote events from the client
-      socket.on('addVote', ({ user_id, card_id, vote_type }) => this.addVote(user_id, card_id, vote_type))
-      socket.on('removeVote', ({ user_id, card_id, vote_type }) => this.removeVote(user_id, card_id, vote_type))
+      socket.on('addVote', ({ user_id, card_id, vote_type }) => this.addVote(retro_id, user_id, card_id, vote_type))
+      socket.on('removeVote', ({ user_id, card_id, vote_type }) => this.removeVote(retro_id, user_id, card_id, vote_type))
     });
   }
 
@@ -54,14 +55,13 @@ module.exports = class SocketServer {
     socket.join(retro_id);
     console.log('User has joined retro. ', { user_id, retro_id })
 
-    // Set our internal retro_id
-    this.retro_id = retro_id
-
     // Send a broadcast to the room that the user has joined
     this.io.to(retro_id).emit('userJoinedRetro', user_id)
 
     // Send that user the retro objects
     await this.sendRetroToUser(socket, retro_id)
+
+    return retro_id
   }
 
   /**
@@ -102,7 +102,7 @@ module.exports = class SocketServer {
 
   async renameColumn(retro_id, column_id, newName) {
     let column = await updateColName(column_id, newName)
-    this.io.to(this.retro_id).emit('columnRenamed', { column })
+    this.io.to(retro_id).emit('columnRenamed', { column })
   }
 
   /**
@@ -112,37 +112,31 @@ module.exports = class SocketServer {
    * @param {*} column_id
    * @param {*} user_id
    */
-  async addCard(column_id, user_id) {
+  async addCard(retro_id, column_id, user_id) {
     await insertNewCard(column_id, user_id)
     let cards = await fetchCardsByColumnId(column_id)
     let column = await fetchColumnById(column_id)
-    this.cardUpdated(cards, column)
+    this.cardUpdated(retro_id, cards, column)
   }
 
-  async removeCard(card_id) {
+  async removeCard(retro_id, card_id) {
     console.log('removeCard', card_id)
     let column_id = await fetchColumnIdByCardId(card_id)
     console.log('column_id', column_id)
     await deleteCard(card_id, column_id)
     let newCards = await fetchCardsByColumnId(column_id)
     let column = await fetchColumnById(column_id)
-    this.cardUpdated(newCards, column)
+    this.cardUpdated(retro_id, newCards, column)
   }
 
-  cardUpdated(cards, column) {
-    this.io.to(this.retro_id).emit('cardUpdated', { cards, column })
+  cardUpdated(retro_id, cards, column) {
+    this.io.to(retro_id).emit('cardUpdated', { cards, column })
   }
 
-  async changeCardText(card_id, card_text) {
+  async changeCardText(retro_id, card_id, card_text) {
     let card = await updateCardText(card_id, card_text)
-    this.io.to(this.retro_id).emit('cardTextUpdated', { card })
+    this.io.to(retro_id).emit('cardTextUpdated', { card })
   }
-
-  //should be card updated once remove card is refactored
-  // cardDeleted(retro_id, cardId) {
-  //   this.io.to(retro_id).emit('cardDeleted', cardId)
-  // }
-
 
   /**
    *
@@ -150,42 +144,42 @@ module.exports = class SocketServer {
    * @param {*} comment_text
    * @param {*} user_id
    */
-  async addComment(card_id, user_id) {
+  async addComment(retro_id, card_id, user_id) {
     console.log('addComment on: ', card_id, user_id)
     await insertComment(card_id, user_id)
     let comments = await fetchCommentsByCardId(card_id)
     let card = await fetchCardByCardId(card_id)
-    this.commentUpdated(card, comments)
+    this.commentUpdated(retro_id, card, comments)
   }
 
-  async removeComment(comment_id, card_id) {
+  async removeComment(retro_id, comment_id, card_id) {
     console.log('removeComment', comment_id, card_id)
     await deleteComment(comment_id)
     let comments = await fetchCommentsByCardId(card_id)
     let card = await fetchCardByCardId(card_id)
-    this.commentUpdated(card, comments)
+    this.commentUpdated(retro_id, card, comments)
   }
 
-  commentUpdated(card, comments) {
-    this.io.to(this.retro_id).emit('commentUpdated', { card, comments })
+  commentUpdated(retro_id, card, comments) {
+    this.io.to(retro_id).emit('commentUpdated', { card, comments })
   }
 
-  async changeCommentText(comment_id, comment_text) {
+  async changeCommentText(retro_id, comment_id, comment_text) {
     let comment = await updateCommentText(comment_id, comment_text)
-    this.io.to(this.retro_id).emit('commentTextUpdated', { comment })
+    this.io.to(retro_id).emit('commentTextUpdated', { comment })
   }
 
-  async addVote(user_id, card_id, vote_type) {
+  async addVote(retro_id, user_id, card_id, vote_type) {
     await updateAddVote(user_id, card_id, vote_type)
     let newCard = await fetchCardByCardId(card_id)
     let votes = newCard.votes
-    this.io.to(this.retro_id).emit('votesChanged', { card_id, votes })
+    this.io.to(retro_id).emit('votesChanged', { card_id, votes })
   }
 
-  async removeVote(user_id, card_id, vote_type) {
+  async removeVote(retro_id, user_id, card_id, vote_type) {
     await updateRemoveVote(user_id, card_id, vote_type)
     let newCard = await fetchCardByCardId(card_id)
     let votes = newCard.votes
-    this.io.to(this.retro_id).emit('votesChanged', { card_id, votes })
+    this.io.to(retro_id).emit('votesChanged', { card_id, votes })
   }
 }
