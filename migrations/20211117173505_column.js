f321787f0f36fc06d334d90285e7073417fa1da6
Mario <matthew.davies.3@spaceforce.mil>
2021-12-02 06:54:01 -0500

    remove comment

--- migrations/20211117173505_column.js
+++ migrations/20211117173505_column.js
@@ -3,7 +3,6 @@ exports.up = function (knex) {
     table.increments('column_id'); // adds an auto incrementing PK column
     table.string('column_name') // name of the column
     table.specificType('card_ids', 'int[]').defaultTo('{}')
-    // table.json('card_ids') // array of card IDs, specifying which cards are in the column, and what order e.g. [1, 3, 2]
     table.timestamps(true, true); // adds created_at and updated_at
   });
 };
